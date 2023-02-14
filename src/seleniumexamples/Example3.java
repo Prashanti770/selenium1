54bd208f28dd6d56a4fd6c1bd11adc54475ac0c8
Prashanti770 <prashanti64m@gmail.com>
2023-02-14 10:21:05 +0530

    mythird commit from eclipse

---
+++ src/seleniumexamples/Example3.java
@@ -0,0 +1,16 @@
+package seleniumexamples;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Example3 {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium webdriver\\ChromeDriver\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		System.out.println("Title of Page is: " + driver.getTitle());
+		driver.getTitle();
+		
+}
+}
